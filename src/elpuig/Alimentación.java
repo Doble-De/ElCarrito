17dba2a98335fb3cd640d1d58ab8fe717b9bf87a
Christian <cruizm97@elpuig.xeill.net>
2018-11-09 19:04:46 +0100

    Añadido de alimentos

--- src/elpuig/Alimentación.java
+++ src/elpuig/Alimentación.java
@@ -1,14 +1,61 @@
 package elpuig;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public class Alimentación {
 
-    Calendar diaactual = new GregorianCalendar();
-    int año =diaactual.get(Calendar.YEAR);
-    int mes =diaactual.get(Calendar.MONTH);
-    int dia =diaactual.get(Calendar.DAY_OF_MONTH);
+    private LocalDate Data;
+    private int dia;
+    private int mes;
+    private int año;
 
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+
+    public LocalDate getData() {
+        return Data;
+    }
+
+    public void setData(LocalDate data) {
+        Data = data;
+    }
+
+
+    public Period getFechaActual(){
+        LocalDate startDateInclusive = LocalDate.now ();
+        LocalDate lastDateInclusive = LocalDate.of(getAño(),getMes(),getDia());
+
+        Period periodo =Period.between(startDateInclusive,lastDateInclusive);
+        periodo.getYears();
+        periodo.getMonths();
+        periodo.getDays();
+        return periodo;
+    }
 
 }
